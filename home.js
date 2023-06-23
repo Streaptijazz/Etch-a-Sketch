e3c6424f21b4d6e3e7dadb2464831c892b30a99e
Streaptijazz <134112215+Streaptijazz@users.noreply.github.com>
2023-06-23 13:13:54 +0300

    modified: home.js

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
-const container = document.querySelector('#container');
+// Get the grid container element
+const gridContainer = document.getElementById("gridContainer");
+
+// Create 16x16 grid divs
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+    // Create a div element for each grid cell
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    div.textContent = `${i + 1}-${j + 1}`;
+
+    // Append the div to the grid container
+    gridContainer.appendChild(div);
+    }
+}
